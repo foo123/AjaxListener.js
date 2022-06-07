361b55b27a6c65132dc1ac90674bc910a4f0b811
Nikos M <workingclasscode@gmail.com>
2022-06-07 18:05:49 +0300

    v.1.0.0 (demo missing)

--- src/AjaxListener.js
+++ src/AjaxListener.js
@@ -14,83 +14,308 @@ if ('function' === typeof define && define.amd) define(function(req) {return roo
 
 var AjaxListener = {VERSION: '1.0.0'},
     callbacks = [], installed = false,
-    FETCH = window.fetch,
-    XHR = window.XMLHttpRequest,
-    xhrOpen, xhrSend
+    HAS = Object.prototype.hasOwnProperty, NL = /[\r\n]+/,
+    fetch = window.fetch, xhr = window.XMLHttpRequest, xhrOpen
 ;
 
-function notify(obj)
+function parse_url_params(params)
 {
-    for (var cb=callbacks.slice(), i=0, n=cb.length; i<n; ++i) cb[i](obj);
+    return String(params).trim().split('&').reduce(function(o, kv){
+        kv = kv.split('=');
+        var key = kv[0].length ? decodeURIComponent(kv[0]) : null,
+            val = kv[1] && kv[1].length ? decodeURIComponent(kv[1]) : true;
+        if (key)
+        {
+            if (HAS.call(o, key))
+            {
+                if (o[key].push) o[key].push(val);
+                else o[key] = [o[key], val];
+            }
+            else
+            {
+                o[key] = val;
+            }
+        }
+        return o;
+    }, {});
 }
+function parse_headers(headers)
+{
+    return String(headers).trim().split(NL).reduce(function(o, line) {
+        line = line.trim();
+        if (line.length)
+        {
+            var parts = line.split(': '), header = parts.shift(), value = parts.join(': ');
+            o[header] = value;
+        }
+        return o;
+    }, {});
+}
+
+function notify(req, res)
+{
+    setTimeout(function() {
+        for (var cb=callbacks.slice(), i=0, n=cb.length; i<n; ++i) cb[i](req, res);
+        req.dispose(); res.dispose();
+    }, 0);
+}
+
+function Request(type)
+{
+    this.type = type;
+}
+Request.prototype = {
+    constructor: Request,
+
+    type: '',
+    _method: 'GET',
+    _url: null,
+    _headers: null,
+    _body: null,
+
+    dispose: function() {
+        this._method = null;
+        this._url = null;
+        this._headers = null;
+        this._body = null;
+        return this;
+    },
+
+    setMethod: function(method) {
+        this._method = String(method).toUpperCase();
+        return this;
+    },
+    getMethod: function() {
+        return this._method;
+    },
+
+    setUrl: function(url) {
+        this._url = url;
+        return this;
+    },
+    getUrl: function(raw) {
+        var url = this._url, parts;
+        if (true === raw) return url;
+        if (url)
+        {
+            parts = url.split('?');
+            if (parts[1])
+            {
+                parts[1] = parts[1].split('#');
+                if (parts[1][1])
+                {
+                    return {url:parts[0], search:parse_url_params(parts[1][0]), hash:parts[1][1]};
+                }
+                else
+                {
+                    return {url:parts[0], search:parse_url_params(parts[1][0]), hash:null};
+                }
+            }
+            else
+            {
+                parts[0] = parts[0].split('#');
+                if (parts[0][1])
+                {
+                    return {url:parts[0][0], search:null, hash:parts[0][1]};
+                }
+                else
+                {
+                    return {url:url, search:null, hash:null};
+                }
+            }
+        }
+    },
+
+    setHeaders: function(headers) {
+        this._headers = headers;
+        return this;
+    },
+    getHeaders: function(raw) {
+        var headers = this._headers;
+        if (true === raw) return headers;
+        if (headers)
+        {
+            return headers.keys && header.get ? Array.from(headers.keys()).reduce(function(o, k) {
+                o[k] = headers.get(k);
+                return o;
+            }, {}) : (headers.substring ? parse_headers(headers) : headers);
+        }
+    },
+
+    setBody: function(body) {
+        this._body = body;
+        return this;
+    },
+    getBody: function(format) {
+        var body = this._body;
+        if (true === format) return body;
+        if (body)
+        {
+            return window.FormData && (body instanceof window.FormData) ? Array.from(body.keys()).reduce(function(o, k) {
+               o[k] = body.getAll(k);
+               return o;
+            }, {}) : ('json' === format ? JSON.parse(body) : ('urlenc' === format ? parse_url_params(body) : body));
+        }
+    }
+};
+AjaxListener.Request = Request;
+
+function Response(type)
+{
+    this.type = type;
+}
+Response.prototype = {
+    constructor: Response,
+
+    type: '',
+    _status: 0,
+    _headers: null,
+    _body: null,
+
+    dispose: function() {
+        this._status = null;
+        this._headers = null;
+        this._body = null;
+        return this;
+    },
+
+    setStatus: function(status) {
+        this._status = status;
+        return this;
+    },
+    getStatus: function() {
+        return this._status;
+    },
+
+    setHeaders: function(headers) {
+        this._headers = headers;
+        return this;
+    },
+    getHeaders: function(raw) {
+        var headers = this._headers;
+        if (true === raw) return headers;
+        if (headers)
+        {
+            return headers.keys && header.get ? Array.from(headers.keys()).reduce(function(o, k) {
+                o[k] = headers.get(k);
+                return o;
+            }, {}) : (headers.substring ? parse_headers(headers) : headers);
+        }
+    },
+
+    setBody: function(body) {
+        this._body = body;
+        return this;
+    },
+    getBody: function() {
+        return this._body;
+    }
+};
+AjaxListener.Response = Response;
+
+
+function listenerFetch(url)
+{
+    return fetch.apply(fetch, arguments).then(function(response) {
+        if (callbacks.length)
+        {
+            var req = new Request('fetch'), res = new Response('fetch');
+            response.text().then(function(responseText) {
+                res
+                    .setStatus(response.status)
+                    .setHeaders(response.headers)
+                    .setBody(responseText)
+                ;
+                if (url instanceof window.Request)
+                {
+                    req
+                        .setMethod(url.method)
+                        .setUrl(url.url)
+                        .setHeaders(url.headers)
+                    ;
+                    url.text().then(function(reqBody) {
+                        req.setBody(reqBody)
+                        notify(req, res);
+                    });
+                }
+                else
+                {
+                    req
+                        .setMethod('GET')
+                        .setUrl(url)
+                    ;
+                    notify(req, res);
+                }
+            });
+        }
+        return response;
+  });
+}
+listenerFetch.ajaxListener = AjaxListener;
+
+function listenerOpen(method, url)
+{
+    var self = this, body = null, headers = {},
+        xhrSend = xhr.prototype.send,
+        xhrSetRequestHeader = xhr.prototype.setRequestHeader;
+
+    self.send = function(payload) {
+        if (arguments.length < 1) payload = null;
+        body = payload;
+        xhrSend.apply(self, arguments);
+    };
+    self.setRequestHeader = function(header, value) {
+        headers[header] = HAS.call(headers, header) ? (headers[header]+', '+String(value)) : String(value);
+        xhrSetRequestHeader.apply(self, arguments);
+    };
+
+    if (arguments.length < 1) method = 'GET';
+    if (arguments.length < 2) url = '';
+
+    self.addEventListener('load', function onLoad() {
+        self.removeEventListener('load', onLoad);
+        if (callbacks.length)
+        {
+            var req = new Request('xhr'), res = new Response('xhr');
+            req
+                .setMethod(method)
+                .setUrl(url)
+                .setHeaders(headers)
+                .setBody(body)
+            ;
+            res
+                .setStatus(self.status)
+                .setHeaders(self.getAllResponseHeaders())
+                .setBody(self.responseText)
+            ;
+            notify(req, res);
+        }
+    });
+    xhrOpen.apply(self, arguments);
+}
+listenerOpen.ajaxListener = AjaxListener;
 
 AjaxListener.install = function() {
     if (!installed)
     {
         installed = true;
-        if (FETCH)
+        if (fetch)
         {
-            window.fetch = function(url, options) {
-                var obj = {type:'fetch', method:'GET', url: url, data: null, status:0, response:'', options:options};
-                return FETCH(url, options).then(function(response) {
-                    if (callbacks.length)
-                    {
-                        response.text().then(function(responseText) {
-                            obj.status = response.status;
-                            obj.response = responseText;
-                            if (url instanceof window.Request)
-                            {
-                                obj.method = url.method.toUpperCase();
-                                obj.url = url.url;
-                                if ('POST' === obj.method)
-                                {
-                                    url.text().then(function(d) {
-                                        obj.data = d;
-                                        notify(obj);
-                                    });
-                                }
-                                else
-                                {
-                                    notify(obj);
-                                }
-                            }
-                            else
-                            {
-                                notify(obj);
-                            }
-                        });
-                    }
-                    return response;
-              });
-            };
+            if (fetch.ajaxListener)
+            {
+                fetch.ajaxListener.uninstall();
+                fetch = window.fetch;
+            }
+            window.fetch = listenerFetch;
         }
-        if (XHR)
+        if (xhr)
         {
-            xhrOpen = XHR.prototype.open;
-            xhrSend = XHR.prototype.send;
-            XHR.prototype.open = function(m, u) {
-                var self = this,
-                    obj = {type:'xhr', method:'GET', url:'', data: null, status:0, response:''};
-                if (arguments.length < 1) m = 'GET';
-                if (arguments.length < 2) u = '';
-                obj.method = String(m).toUpperCase();
-                obj.url = u;
-                self.__ = obj;
-                self.addEventListener('load', function onLoad() {
-                    self.removeEventListener('load', onLoad);
-                    self.__ = null;
-                    obj.status = self.status;
-                    obj.response = self.responseText;
-                    if (callbacks.length) notify(obj);
-                });
-                xhrOpen.apply(self, arguments);
-            };
-            XHR.prototype.send = function(d) {
-                var self = this, obj = self.__ || {};
-                if (arguments.length < 1) d = '';
-                if ('POST' === obj.method) obj.data = d;
-                xhrSend.apply(self, arguments);
-            };
+            if (xhr.prototype.open.ajaxListener)
+            {
+                xhr.prototype.open.ajaxListener.uninstall();
+            }
+            xhrOpen = xhr.prototype.open;
+            xhr.prototype.open = listenerOpen;
         }
     }
     return AjaxListener;
@@ -103,10 +328,9 @@ AjaxListener.isInstalled = function() {
 AjaxListener.uninstall = function() {
     if (installed)
     {
-        if (FETCH) window.fetch = FETCH;
-        if (XHR) {XHR.prototype.open = xhrOpen; XHR.prototype.send = xhrSend;}
+        if (fetch) window.fetch = fetch;
+        if (xhr) xhr.prototype.open = xhrOpen;
         xhrOpen = null;
-        xhrSend = null;
         installed = false;
     }
     return AjaxListener;
@@ -118,11 +342,13 @@ AjaxListener.onRequest = function(callback, add) {
         var i = callbacks.indexOf(callback);
         if (false === add)
         {
-            if (-1 !== i) callbacks.splice(i, 1);
+            if (-1 !== i)
+                callbacks.splice(i, 1);
         }
         else
         {
-            if (-1 === i) callbacks.push(callback);
+            if (-1 === i)
+                callbacks.push(callback);
         }
     }
     return AjaxListener;
